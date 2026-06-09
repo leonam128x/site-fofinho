e0213644cbe21ce460b114b4a8e38b2739c5a015
leonam128x <leonamillgnerpereiraramos1@gmail.com>
2026-06-09 10:58:22 -0300

    Update 2 script.js

--- script.js
+++ script.js
@@ -4,26 +4,16 @@ const resultado = document.getElementById("resultado");
 const botoes = document.getElementById("botoes");
 const titulo = document.getElementById("titulo");
 
-const botaoNao = document.getElementById("nao");
+botaoNao.addEventListener("mouseover", () => {
 
-function fugir() {
-    const largura = window.innerWidth - botaoNao.offsetWidth;
-    const altura = window.innerHeight - botaoNao.offsetHeight;
+    const largura = window.innerWidth - 150;
+    const altura = window.innerHeight - 100;
 
     const x = Math.random() * largura;
     const y = Math.random() * altura;
 
-    botaoNao.style.left = x + "px";
-    botaoNao.style.top = y + "px";
-}
-
-// Computador
-botaoNao.addEventListener("mouseover", fugir);
-
-// Celular
-botaoNao.addEventListener("touchstart", function(e) {
-    e.preventDefault();
-    fugir();
+    botaoNao.style.left = `${x}px`;
+    botaoNao.style.top = `${y}px`;
 });
 
 botaoSim.addEventListener("click", () => {
